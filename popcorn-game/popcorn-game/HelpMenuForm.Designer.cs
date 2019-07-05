856252a4c6e13a8468462bd687805652ef677dd1
Dina Gitova <gitova.dina@students.finki.ukim.mk>
2019-07-05 19:39:28 +0200

    Change in Help text

--- popcorn-game/popcorn-game/HelpMenuForm.Designer.cs
+++ popcorn-game/popcorn-game/HelpMenuForm.Designer.cs
@@ -76,7 +76,7 @@
             this.label2.ForeColor = System.Drawing.SystemColors.Control;
             this.label2.Location = new System.Drawing.Point(105, 247);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(327, 143);
+            this.label2.Size = new System.Drawing.Size(327, 165);
             this.label2.TabIndex = 4;
             this.label2.Text = resources.GetString("label2.Text");
             // 
